485b73f50f2eb74010cb49a53f7b7dd08b9e8fac
mampi <mampi.bhakta@envertis.com.au>
2019-12-26 16:08:08 +0530

    'sagnik'

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Super Man' },
+  { id: 12, name: 'BatMan' },
+  { id: 13, name: 'Krish' },
+  { id: 14, name: 'Dr-Thief' },
+  { id: 15, name: 'Magneto' },
+  { id: 16, name: 'Wolverine' },
+  { id: 17, name: 'Rock' },
+  { id: 18, name: 'Spiderman' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Captain-American' },
+  { id: 21, name: 'Sagnik Dey'}
+];
